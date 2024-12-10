524dd802d945ed5ce92c078273f2ccfe7c2bbd71
Umair Ali <umair@dataspecc.com>
2024-12-10 22:58:04 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
+// const port = process.env.PORT || 5000;
+// app.listen(port, () => {
+//   /* eslint-disable no-console */
+//   console.log(`Listening: http://localhost:${port}`);
+//   /* eslint-enable no-console */
+// });
+
+import app from "./app.js";
 import express from "express";
 import bodyParser from "body-parser";
 import companion from "@uppy/companion";
@@ -77,6 +85,3 @@ app.use((err, req, res, next) => {
     .status(err.status || 500)
     .json({ message: err.message || "Internal Server Error" });
 });
-
-// Export the Express app
-export default app;
